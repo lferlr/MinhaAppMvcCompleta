942f97063a99b12415c95b364e4de780ec117909
Lucas Rodrigues <rodrigues14set@hotmail.com>
2021-11-24 23:47:55 -0300

    Organizando e otimizando a estrutura da App

---
+++ src/DevIO.App/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,27 @@
+﻿using DevIO.Data.Context;
+using DevIO.App.Extensions;
+using DevIO.Data.Repository;
+using DevIO.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevIO.App.Configurations
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependecies(this IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddRazorPages();
+
+            services.AddScoped<MeuDbContext>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            services.AddSingleton<IValidationAttributeAdapterProvider, MoedaValidationAttributeAdapterProvider>();
+
+            return services;
+        }
+    }
+}
