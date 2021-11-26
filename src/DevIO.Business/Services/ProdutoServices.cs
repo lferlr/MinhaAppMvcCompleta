3e6b28f5c0dc2395134d092e7d4e3ae0a04de92d
Lucas Rodrigues <rodrigues14set@hotmail.com>
2021-11-25 23:16:42 -0300

    Validando as entidades de negócio

---
+++ src/DevIO.Business/Services/ProdutoServices.cs
@@ -0,0 +1,26 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Business.Models.Validations;
+using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Services
+{
+    public abstract class ProdutoServices : BaseServices, IProdutoServices
+    {
+        public async Task Adicionar(Produto produto)
+        {
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+        }
+
+        public async Task Atualizar(Produto produto)
+        {
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+        }
+
+        public Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
